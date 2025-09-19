2c9b372f28986ebffcfd2126be8df7938f9b7257
KrunalSheth10 <shethkrunal40@gmail.com>
2025-09-18 21:30:58 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_providers {
-    azurerm = {
+    azurerm   = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
@@ -14,4 +14,22 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "krunal-rg"
   location = "East US"
+}
+
+resource "azurerm_storage_account" "storage"{
+  name                      = "krunalstorageacct" # must be globally unique, lowercase only
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_storage_container" "container"{
+  name = "krunal-storage-container"
+  storage_account_name = azurerm_storage_account.storage.name
+  container_access_type = "private"
 }
