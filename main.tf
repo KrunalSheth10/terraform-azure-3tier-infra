bdebb1a347eee60098c6e7ce7ac5ec7c215a73cf
KrunalSheth10 <shethkrunal40@gmail.com>
2025-09-19 21:18:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_resource_group" "rg" {
   name     = "krunal-rg"
   location = "East US"
 }
-
+# storage account
 resource "azurerm_storage_account" "storage"{
   name                      = "krunalstorageacct" # must be globally unique, lowercase only
   resource_group_name       = azurerm_resource_group.rg.name
@@ -28,8 +28,43 @@ resource "azurerm_storage_account" "storage"{
   }
 }
 
+# Storage container
 resource "azurerm_storage_container" "container"{
   name = "krunal-storage-container"
   storage_account_name = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
+
+# Virtual Network
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "krunal-vnet"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space = ["10.0.0.0/16"]
+}
+
+# Web Subnet
+resource "azurerm_subnet" "web"{
+  name = "web-subnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+# App Subnet 
+resource "azurerm_subnet" "app"{
+  name = "app-subnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes =["10.0.2.0/24"]
+}
+
+# DataBase Subnet
+resource "azurerm_subnet" "db" {
+  name = "db-subnet"
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes = ["10.0.3.0/24"]
+}
+
